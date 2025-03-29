c5a7c80ade14f99cb323720e8d2808892e4b44e9
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-03-29 01:40:23 +0100

    update the api docs, and update the film form and series form

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(url('/admin'));
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
