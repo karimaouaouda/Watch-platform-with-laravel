b2150c65cda27791b8b0b61a9b2c4f43c2cee167
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-03-29 02:53:18 +0100

    solved

--- app/Http/Controllers/LicenseController.php
+++ app/Http/Controllers/LicenseController.php
@@ -38,7 +38,6 @@ class LicenseController extends Controller
         }
 
         if($code->device_id == null){
-            dd($code->device_id, $device_unique_id);
             $code->device_id = $device_unique_id;
             $code->status = CodeStatus::ACTIVE->name;
             $code->save();
